49a4bf6d2f37c76b781ed9e0e854abe9b10f914b
syedsoheal <soheal.study@gmail.com>
2026-04-03 05:38:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_id = 10000
+let account_email = "soheal@cloud"
+var account_password = 12345678
+
+let accountstate;
+
+account_email="syed@mail"
+account_password=3456787654
+
+/*prefering not to use the var because of some errors*/
+
+console.log(account_id)
+console.table([account_id , account_email , account_password , accountstate ])
